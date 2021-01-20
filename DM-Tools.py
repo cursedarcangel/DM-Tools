66dca41df379b82e19e3b93f689f350b085a937b
cursedarcangel <71993617+cursedarcangel@users.noreply.github.com>
2021-01-20 11:30:08 -0500

    Adding the start of the first round of tools

--- DM-Tools.py
+++ DM-Tools.py
@@ -2,30 +2,39 @@ from tkinter import *
 
 #Defining the functions for the individual tools
 def magicItem():
-    print('Tracking magic items')
+    from magicItemTracker import magicItemTracker as mIT
+    mIT.magicItemTracker()
 
 def initiative():
-    print('Initiating tracker')
+    from initiativeTracker import initiativeTracker as iT
+    iT.initiativeTracker()
 
 def dice():
-    print('Hey a 20!')
+    from diceRoller import diceRoller as dR
+    dR.diceRoller()
 
 def monster():
-    print('Rawr a monster is coming')
+    from monsterTracker import monsterTracker as mT
+    mT.monsterTracker()
+
+def character():
+    from characterTracker import characterTracker as cT
+    cT.characterTracker()
 
 #The window variable
 window = Tk()
+window.title('DM Tools')
+window.geometry('200x150')
 
 #The title
-name = StringVar()
-title = Label(window, textvariable=name, relief=RAISED)
-name.set("The DM's toolbox")
+title = Label(window, text="The DM's toolbox", relief=RAISED)
 
 #Declaring the buttons for each of the tools
 magicItemTracker = Button(window, text='Magic Item Tracker', command=magicItem)
 initiativeTracker = Button(window, text='Initiative Tracker', command=initiative)
 diceRoller = Button(window, text='Roll the dice...', command=dice)
 monsterTracker = Button(window, text='Monster Tracker', command=monster)
+characterTracker = Button(window, text='Character Tracker', command=character)
 
 #Packing stuff
 title.pack()
---
+++ characterTracker/characterTracker.py
@@ -0,0 +1,6 @@
+from tkinter import *
+
+window = Tk()
+window.title('Character Tracker')
+
+window.mainloop()
