7f68fedc2ddefa21ee1e15da82a40352488597db
cursedarcangel <71993617+cursedarcangel@users.noreply.github.com>
2021-01-20 12:40:53 -0500

    Getting started on the initiative tracker

--- initiativeTracker/initiativeTracker.py
+++ initiativeTracker/initiativeTracker.py
@@ -1,6 +1,14 @@
 from tkinter import *
 
+def add():
+    print('Thing added')
+
 window = Tk()
 window.title('Initiative Tracker')
+window.geometry('300x300')
+
+addThing = Button(window, text='Add a thing to initiative', command=add)
+
+addThing.place(x=60, y=250)
 
 window.mainloop()
