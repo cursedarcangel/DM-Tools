f68df20edb3d3c7c1e078b44cd6fcb2a9072b82e
cursedarcangel <71993617+cursedarcangel@users.noreply.github.com>
2021-01-21 19:45:03 -0500

    Dice roller app

--- diceRoller/diceRoller.py
+++ diceRoller/diceRoller.py
@@ -1,7 +1,32 @@
 from tkinter import *
+from random import randint
 
 def diceRoller():
     window = Tk()
     window.title('Dice Roller')
+
+    reslt = StringVar()
+
+    dNum = Entry(window)
+    numD = Entry(window)
+
+    result = Label(window, textvariable=reslt, relief=RAISED)
+
+    def Roll():
+        total = 0
+        dNumVal = int(dNum.get())
+        numDVal = int(numD.get())
+        
+        for i in range(numDVal):
+            total += randint(1, dNumVal)
+        
+        print(total)
+
+    roll = Button(window, text='Roll', command=Roll)
+    
+    dNum.pack()
+    numD.pack()
+    roll.pack()
+    result.pack()
 
     window.mainloop()
